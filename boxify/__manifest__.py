9600966a167cf2a1e5266a306e4222e7df7be62f
alay <alayshah.tecblic@gmail.com>
2024-06-21 12:32:28 +0530

    [ADD][boxify]module developed

---
+++ boxify/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Dakshinamurti Boxes',
+    'version': '1.8',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'This custom Odoo module, developed in Odoo version 17 Enterprise, is designed to streamline operations for corrugated box manufacturers. It includes comprehensive features for managing production, inventory, sales, logistics, and accounting. The module offers real-time tracking of production stages, automated stock updates, a user-friendly interface for order processing, integrated shipping management, and detailed reporting and analytics. Tailored to the industry unique needs, it improves operational efficiency, reduces costs, and enhances customer satisfaction',
+    'website': 'alayshah.in',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
